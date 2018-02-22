2fbba65d3ce94c035e570ebcd5671e86c847e657
Ovan Crone <ovan.crone@skykick.com>
2018-02-21 18:14:32 -0800

    :sparkles: Initial commit!

---
+++ src/Psibr.Linq.ForEach/EnumerableForEachExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+#if !NET40
+using System.Threading.Tasks;
+#endif
+
+// ReSharper disable once CheckNamespace
+namespace System.Linq
+{
+    public static class EnumerableForEachExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action(item);
+            }
+        }
+
+        public static void ForEach(this IEnumerable enumerable, Action<object> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action(item);
+            }
+        }
+
+#if !NET40
+        public static async Task ForEachAsync<T>(this IEnumerable<T> enumerable, Func<T, Task> action)
+        {
+            foreach (var item in enumerable)
+            {
+                await action(item);
+            }
+        }
+#endif
+    }
+}
